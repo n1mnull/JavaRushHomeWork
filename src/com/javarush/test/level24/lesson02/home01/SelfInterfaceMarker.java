3aae85b71f4ce16be4f396c6e274a6edc39ef5a9
Alenka <3980039@gmail.com>
2016-04-11 00:02:42 +0300

    JR level24.lesson02.home01 выполнено 10.04.16

---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by Alenka on 10.04.2016.
+ */
+public interface SelfInterfaceMarker
+{
+}
---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by Alenka on 10.04.2016.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker
+{
+    public SelfInterfaceMarkerImpl() {}
+
+    public void print() {}
+
+    public void read() {}
+}
