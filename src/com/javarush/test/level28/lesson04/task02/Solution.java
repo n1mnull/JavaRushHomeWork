2081a5368694ffd70242ddbddeabf75eb123c0eb
Alenka <3980039@gmail.com>
2016-04-22 16:42:01 +0300

    JR level28.lesson04.task02 выполнено 21.04.16

--- src/com/javarush/test/level28/lesson04/task02/Solution.java
+++ src/com/javarush/test/level28/lesson04/task02/Solution.java
@@ -10,19 +10,16 @@ getRandomDouble должен возвращать случайный double
 getRandomLongBetween0AndN должен возвращать случайный long между 0 и n
 */
 public class Solution {
-    public static int getRandomIntegerBetweenNumbers(int from, int to)
-    {
+    public static int getRandomIntegerBetweenNumbers(int from, int to) {
 
         return ThreadLocalRandom.current().nextInt(from, to);
     }
 
-    public static double getRandomDouble()
-    {
+    public static double getRandomDouble() {
         return ThreadLocalRandom.current().nextDouble();
     }
 
-    public static long getRandomLongBetween0AndN(long n)
-    {
-        return ThreadLocalRandom.current().nextLong(0, n);
+    public static long getRandomLongBetween0AndN(long n) {
+        return ThreadLocalRandom.current().nextLong(n);
     }
 }
