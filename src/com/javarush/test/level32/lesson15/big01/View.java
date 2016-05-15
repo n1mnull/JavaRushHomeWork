c77ae1d9da62d1d538a535c325d0b92e58dd08f7
Alenka <3980039@gmail.com>
2016-05-16 00:04:16 +0300

    JR level32.lesson15.big01 выполнено 16.05.16

--- src/com/javarush/test/level32/lesson15/big01/View.java
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -7,8 +7,25 @@ import java.awt.event.ActionListener;
 /**
  * Created by Alenka on 15.05.2016.
  */
-public class View extends JFrame implements ActionListener
-{
+public class View extends JFrame implements ActionListener {
+
+    private Controller controller;
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void init() {
+
+    }
+
+    public void exit() {
+        controller.exit();
+    }
 
     @Override
     public void actionPerformed(ActionEvent e)
