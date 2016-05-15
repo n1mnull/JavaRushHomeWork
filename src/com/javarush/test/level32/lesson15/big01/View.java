38d289676752ea8de04733a344110793e6a99f32
Alenka <3980039@gmail.com>
2016-05-15 23:56:42 +0300

    JR level32.lesson15.big01 выполнено 15.05.16

---
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level32.lesson15.big01;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Alenka on 15.05.2016.
+ */
+public class View extends JFrame implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+}
