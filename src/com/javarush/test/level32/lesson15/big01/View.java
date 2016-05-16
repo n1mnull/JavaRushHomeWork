e6777718275e9f798f7a840205a715016ebcd4cc
Alenka <3980039@gmail.com>
2016-05-17 00:28:29 +0300

    JR level32.lesson15.big01 выполнено 17.05.16

--- src/com/javarush/test/level32/lesson15/big01/View.java
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -1,8 +1,10 @@
 package com.javarush.test.level32.lesson15.big01;
 
 import com.javarush.test.level32.lesson15.big01.listeners.FrameListener;
+import com.javarush.test.level32.lesson15.big01.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -43,7 +45,16 @@ public class View extends JFrame implements ActionListener {
 
     public void initMenuBar() {}
 
-    public void initEditor() {}
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollHtml = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", jScrollHtml);
+        JScrollPane jScrollPlain = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", jScrollPlain);
+        tabbedPane.setPreferredSize(new Dimension(800, 600));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
 
     public void initGui() {
         initMenuBar();
