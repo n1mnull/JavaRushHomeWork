7aa07f51f2bffd9ee4dadbf12625f346bc948744
Alenka <3980039@gmail.com>
2016-05-20 23:51:41 +0300

    JR level31.lesson15.big01 выполнено 20.05.16

--- src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
+++ src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -1,8 +1,20 @@
 package com.javarush.test.level31.lesson15.big01;
 
+import java.nio.file.Path;
+
 /**
  * Created by Alenka on 19.05.2016.
  */
 public class ZipFileManager
 {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile)
+    {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception {
+
+    }
 }
