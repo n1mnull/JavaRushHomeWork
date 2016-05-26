2abba471c04a60257e2dc984c500d242f65de4c8
Alenka <3980039@gmail.com>
2016-05-26 13:02:55 +0300

    JR level33.lesson15.big01 выполнено 26.05.16

---
+++ src/com/javarush/test/level33/lesson15/big01/ExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level33.lesson15.big01;
+
+/**
+ * Created by Alenka on 26.05.2016.
+ */
+public class ExceptionHandler {
+
+    public static void log(Exception e){
+        Helper.printMessage(e.toString());
+    }
+}
